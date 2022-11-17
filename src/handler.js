e02694f77e52ee5ce34550325c5f8fe81b966553
juandaantoniusapakpahan <jujupapa06@gmail.com>
2022-11-17 10:43:36 +0700

    change getnodebyId

--- src/handler.js
+++ src/handler.js
@@ -46,7 +46,7 @@ const getNoteByIdHandler = (request, h) => {
   const { id } = request.params;
   const note = notes.filter((n) => n.id === id)[0];
 
-  if (note !== undefined) {
+  if (`${note}` !== 'undefined') {
     return {
       status: 'success',
       message: 'Catatan berhasil ditemukan',
